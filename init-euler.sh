4d31a4716b5e605ffee3486272cbe081b97acdf6
sven-weber <mail@sven-weber.net>
2024-10-25 11:45:48 +0200

    Adding pip to script

--- init-euler.sh
+++ init-euler.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Load the needed euler modules
-module load stack/2024-05 gcc/13.2.0 openmpi/4.1.6 python/3.11.6
+module load stack/2024-05 gcc/13.2.0 openmpi/4.1.6 python/3.11.6 pip/23.2.1
