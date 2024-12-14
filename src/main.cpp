a026838981bcf5539b22aaefb0d1cb6289f69a52
Sven Weber <mail@sven-weber.net>
2024-12-14 22:48:11 +0100

    Adding section data

--- src/main.cpp
+++ src/main.cpp
@@ -120,14 +120,15 @@ int main(int argc, char **argv) {
       std::cout << "Computing shuffling since no existing one could be found"
                 << std::endl;
       // No shuffle
-      std::iota(A_shuffle.begin(), A_shuffle.end(), 0);
-      std::iota(B_shuffle.begin(), B_shuffle.end(), 0);
+      // std::iota(A_shuffle.begin(), A_shuffle.end(), 0);
+      // std::iota(B_shuffle.begin(), B_shuffle.end(), 0);
 
       // Random shuffle
       //A_shuffle = partition::shuffle(A_fields.height);
       //B_shuffle = partition::shuffle(B_fields.width);
+
       // Perform the shuffling if no persistet one exists!
-      //partition::iterative_shuffle(C_sparsity_path, &A_shuffle, &B_shuffle);
+      partition::iterative_shuffle(C_sparsity_path, &A_shuffle, &B_shuffle);
       //partition::save_shuffle(A_shuffle, A_shuffle_path);
       //partition::save_shuffle(B_shuffle, B_shuffle_path);
     }
