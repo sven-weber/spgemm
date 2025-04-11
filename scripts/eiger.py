2851528e96b424b6980404b0569ebddaac53a4b6
Nabil Abubaker <nabubake@eiger-ln003.cscs.ch>
2025-04-11 13:13:12 +0200

    slurm-related edits, nsec changed

--- scripts/eiger.py
+++ scripts/eiger.py
@@ -12,14 +12,17 @@ CMD = "/build/dphpc"
 CMD = os.path.join(SCRATCH, "spgemm/build/dphpc")
 RUNS_DIR = "runs"
 RUNS_DIR = os.path.join(SCRATCH, "spgemm/runs")
+N_SEC = 131072 
 
-cpus_per_machine = 64
-threads_per_mpi = 16
+cpus_per_machine = 256
+threads_per_mpi = 32
+mpi_per_machine = 16
+
 MPI_OPEN_MP_CONFIG = [
     {"nodes": (16 * threads_per_mpi) // cpus_per_machine, "mpi": 16},
     {"nodes": (64 * threads_per_mpi) // cpus_per_machine, "mpi": 64},
-    {"nodes": (256 * threads_per_mpi) // cpus_per_machine, "mpi": 256},
-    #{"nodes": (512 * threads_per_mpi) // cpus_per_machine, "mpi": 512},
+    {"nodes": (256 // mpi_per_machine), "mpi": 256},
+    {"nodes": (1024 // mpi_per_machine), "mpi": 1024},
 ]
 
 def render_batch_script(impls, matrices, mpi, nodes, last_job=None):
@@ -27,6 +30,7 @@ def render_batch_script(impls, matrices, mpi, nodes, last_job=None):
         mpi = mpi
 
     processes_per_mpi = (nodes * cpus_per_machine) // mpi
+    threads_per_mpi = cpus_per_machine // mpi_per_machine
     n_switches = 1 if nodes <= 256 else 2
     date = datetime.now().strftime("%Y-%m-%d-%H%M%S")
     log_out = f"{RUNS_DIR}/slurm_%x_%j.out"
@@ -53,7 +57,9 @@ def render_batch_script(impls, matrices, mpi, nodes, last_job=None):
         switches=n_switches,
         log_out=log_out,
         err_out=err_out,
-        last_job=last_job
+        last_job=last_job,
+        nsec = N_SEC,
+        mpi_per_node=mpi_per_machine
     )
 
     #job_path = f"jobs/job_{nodes}_{mpi}{"_comb3d" if "comb3d" in impls else ""}.sh"
@@ -67,6 +73,7 @@ def render_batch_script(impls, matrices, mpi, nodes, last_job=None):
 def main():
     matrices = ["dielFilterV2real", "stokes", "HV15R", "nlpkkt200", "nlpkkt240", "GAP-twitter"]
     matrices = '"dielFilterV2real stokes HV15R nlpkkt200 nlpkkt240 GAP-twitter"'
+    matrices = '"dielFilterV2real stokes HV15R nlpkkt200 nlpkkt240"'
 
     impls = ["comb1d", "comb2d", "drop_parallel"]
     impls = '"comb1d comb2d drop_parallel"'
